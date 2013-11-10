455fb36d210ac6113f67b807d9d83f8e65872131
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-11-10 18:03:45 +0000

    Working text parsing.
    Sadly, the filesize of the data file is bigger than the JSON one (hence less characters…)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var fs = require('fs');
 var tld = require('./lib/tld.js').init();
-tld.rules = require('./dist/rules.json');
+
+tld.updateFromText(fs.readFileSync('./dist/rules.txt').toString());
 
 module.exports = tld;
