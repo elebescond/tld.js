271a129922e58d8fd12e5b50ea0eefe4adc43aff
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-11-11 17:30:16 +0000

    Benchmarking various implementations of rule parsing

--- bin/benchmark.js
+++ bin/benchmark.js
@@ -7,10 +7,6 @@ var rules = {
   json: require(__dirname + '/../dist/rules.json'),
   text: fs.readFileSync(__dirname + '/../dist/rules.txt').toString()
 };
-var instances = {
-  json: null,
-  text: null
-};
 
 var onComplete = function onComplete() {
   console.log('Fastest', this.name, 'is ' + this.filter('fastest').pluck('name'));
@@ -18,6 +14,28 @@ var onComplete = function onComplete() {
 var onCycle = function onCycle(event) {
   console.log(String(event.target));
 };
+
+function withJsonData(config){
+  var tld = Tld.init();
+
+  return Benchmark.extend(config, {
+    fn: config.fn(tld),
+    onStart: function(){
+      tld.rules = rules.json;
+    }
+  });
+}
+
+function withTextData(config){
+  var tld = Tld.init();
+
+  return Benchmark.extend(config, {
+    fn: config.fn(tld),
+    onStart: function(){
+      tld.updateFromText(rules.text);
+    }
+  });
+}
 
 /*
  Constructor
@@ -34,3 +52,67 @@ var onCycle = function onCycle(event) {
   .on('cycle', onCycle)
   .on('complete', onComplete)
   .run({ async: true });
+
+
+(new Benchmark.Suite('Tld w/o Sld'))
+  .add(withJsonData({
+    name: 'json',
+    fn: function(tld){
+      return function(){
+        return tld.getRulesForTldJSON('am');
+      }
+    }
+  }))
+  .add(withTextData({
+    name: 'text',
+    fn: function(tld){
+      return function(){
+        tld.getRulesForTld('am');
+      }
+    }
+  }))
+  .on('cycle', onCycle)
+  .on('complete', onComplete)
+  .run({ async: true });
+
+(new Benchmark.Suite('Tld with regular Sld'))
+  .add(withJsonData({
+    name: 'json',
+    fn: function(tld){
+      return function(){
+        return tld.getRulesForTldJSON('com');
+      }
+    }
+  }))
+  .add(withTextData({
+    name: 'text',
+    fn: function(tld){
+      return function(){
+        tld.getRulesForTld('com');
+      }
+    }
+  }))
+  .on('cycle', onCycle)
+  .on('complete', onComplete)
+  .run();
+
+(new Benchmark.Suite('Tld with a lot of rules Sld'))
+  .add(withJsonData({
+    name: 'json',
+    fn: function(tld){
+      return function(){
+        return tld.getRulesForTldJSON('jp');
+      }
+    }
+  }))
+  .add(withTextData({
+    name: 'text',
+    fn: function(tld){
+      return function(){
+        tld.getRulesForTld('jp');
+      }
+    }
+  }))
+  .on('cycle', onCycle)
+  .on('complete', onComplete)
+  .run();
